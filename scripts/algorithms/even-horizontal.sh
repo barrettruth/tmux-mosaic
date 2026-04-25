3a275e99053d9290da14d9d10039326160d78b2d
Barrett Ruth <62671086+barrettruth@users.noreply.github.com>
2026-04-25 14:13:35 -0400

    refactor: share common algorithm helpers (#22)

--- scripts/algorithms/even-horizontal.sh
+++ scripts/algorithms/even-horizontal.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-algo_relayout() {
-  local win="${1:-}"
-  [[ -z "$win" ]] && win=$(tmux display-message -p '#{window_id}')
+algo_relayout() { mosaic_relayout_simple even-horizontal "${1:-}"; }
 
-  if ! mosaic_enabled "$win"; then
-    mosaic_log "relayout: disabled on $win, skipping"
-    return 0
-  fi
-
-  local n
-  n=$(tmux list-panes -t "$win" 2>/dev/null | wc -l)
-  [[ "$n" -le 1 ]] && return 0
-
-  tmux select-layout -t "$win" even-horizontal 2>/dev/null || true
-
-  mosaic_log "relayout: win=$win n=$n"
-}
-
-algo_toggle() {
-  local win
-  win=$(tmux display-message -p '#{window_id}')
-  if mosaic_enabled "$win"; then
-    tmux set-option -wqu -t "$win" "@mosaic-enabled" 2>/dev/null
-    tmux display-message "mosaic: off"
-  else
-    tmux set-option -wq -t "$win" "@mosaic-enabled" 1
-    tmux display-message "mosaic: on"
-    algo_relayout "$win"
-  fi
-}
+algo_toggle() { mosaic_toggle_window algo_relayout; }
